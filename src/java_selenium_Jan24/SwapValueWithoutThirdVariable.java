14fe21c9c7153e29ca406b414708857ed8c372fc
shyam singh <shyamsingh300982@gmail.com>
2024-03-23 13:34:22 +0530

    Swap Value Without Third Variable

---
+++ src/java_selenium_Jan24/SwapValueWithoutThirdVariable.java
@@ -0,0 +1,19 @@
+package java_selenium_Jan24;
+
+public class SwapValueWithoutThirdVariable {
+	
+public static void main(String[] args) {
+		
+		int i = 10;
+		int j = 7;
+			i =i%j;
+			j = i+j;
+			i=j-i;
+				
+	System.out.println(i);
+	System.out.println(j);		
+	}
+	}
+	
+	
+
