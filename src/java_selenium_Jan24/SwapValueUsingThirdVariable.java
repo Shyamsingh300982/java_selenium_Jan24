dc87819c8b9e817b0163ed3ef06dc0900c7c24b1
shyam singh <shyamsingh300982@gmail.com>
2024-03-23 12:59:25 +0530

    Swap value by using third variable

--- src/java_selenium_Jan24/SwapValueUsingThirdVariable.java
+++ src/java_selenium_Jan24/SwapValueUsingThirdVariable.java
@@ -1,5 +1,17 @@
 package java_selenium_Jan24;
 
 public class SwapValueUsingThirdVariable {
+	public static void main(String[] args) {
+		
+		int i = 10;
+		int j = 7;
+		int k = i;
+			i = j;
+			j = k;
+				
+	System.out.println(i);
+	System.out.println(j);		
+	}
+	}
+	
 
-}
