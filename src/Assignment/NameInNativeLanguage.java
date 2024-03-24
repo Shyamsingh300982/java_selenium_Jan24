a458e318209b70e8ce2b0c66cd54c60b5edd0627
shyam singh <shyamsingh300982@gmail.com>
2024-03-24 23:58:32 +0530

    Name in native language

---
+++ src/Assignment/NameInNativeLanguage.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+class NameInNativeLanguage {
+		public static void main(String[] args) {
+		
+			char ch = '\u0936';
+			char ch1 = '\u094D';
+			char ch2 = '\u092F';
+			char ch3 = '\u093E';
+			char ch4 = '\u092E';
+			
+			System.out.println(ch+""+ch1+ch2+ch3+ch4);
+					
+		}
+		}
