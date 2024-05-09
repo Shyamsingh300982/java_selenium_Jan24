f947ec4f6b7101aa225a2c1453b31eb62ef79d27
shyam singh <shyamsingh300982@gmail.com>
2024-05-09 14:17:57 +0530

    3 ways to write infinite loop

---
+++ src/Assignment/WaysOfInfiniteLoop3Type.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+public class WaysOfInfiniteLoop3Type {
+public static void main (String[]args) {
+
+// 1st infinite loop by using while loop
+
+		 while (true) {  	 
+	     System.out.println("Hello World");
+	 }}}
+
+	
+// 2nd infinite loop by using for loop
+		for (int i=0 ; ; i++) {  
+		System.out.println("Hello World ");
+		}}}
+	
+	// 3rd infinite loop by using do.while loop
+	do {							
+    System.out.println("Hello World");
+	} while (true);
+	}}
+
